d269adf2e47f0225aa804a1e5511126f4cba0703
SPG-Jamil <spidergrey.jameel@gmail.com>
2017-10-30 16:00:40 +0500

    TipCalculator.

---
+++ TestCalculatorOnGIT/TipCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TestCalculatorOnGIT
+{
+    public class TipCalculator
+    {
+        private const double TipRate = 0.18;
+
+        public static int Main(string[] args)
+        {
+            double billTotal;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("usage: TIPCALC total");
+                return 1;
+            }
+            if (!double.TryParse(args[0], out billTotal))
+            {
+                Console.WriteLine("usage: TIPCALC total");
+                return 1;
+            }
+            var tip = billTotal*TipRate;
+            Console.WriteLine();
+            Console.WriteLine("Bill total:\t{0,8:c}", billTotal);
+            Console.WriteLine("Tip total/rate:\t{0,8:c} ({1:p1})", tip, TipRate);
+            Console.WriteLine("".PadRight(24, '-'));
+            Console.WriteLine("Grand total:\t{0,8:c}", billTotal + tip);
+            return 0;
+        }
+    }
+}
